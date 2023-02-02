43204dd6806a19bf49f5d56e257ce75efd675be2
Dp7611 <dp761@student.aru.ac.uk>
2023-02-02 00:52:22 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace learningcsharp3;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string username;
+
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Please enter yor name");
+        username = Console.ReadLine();
+        Console.WriteLine($"Goodbye, {username}!");
+        Console.ReadKey(true);
+    }
+}
